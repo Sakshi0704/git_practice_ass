352e857502b8de36566670e1c52b572b874a615b
Ananyakumarisingh <ananyakumarisingh77@gmail.com>
2022-09-14 16:05:29 +0530

    updated prime and palindrome solution

--- palindrome_problem.js
+++ palindrome_problem.js
@@ -1,13 +1,11 @@
 // Problem2 : Check whether a string is palindrome or not.
-
-let str="naman";
-let str2="";
-
-for(let i=str.lenght;i>=0;i++)
-	{
-		str2.push(str1[i]);
-	}
-if(str1==str2)
-	console.log("Yes");
-esle
-	conosle.log("No");
+let i;
+let str = "kakaakak";
+let k=str.length-1;
+for( i=0; i<str.length; i++){
+if (str[i] != str[k]){ 
+break;
+} k--;}
+if(i==str.length){ console.log("Yes");
+}else{
+console.log("No");}
